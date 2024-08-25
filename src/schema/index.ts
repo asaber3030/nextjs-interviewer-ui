b98e620ee12c2f30eb6fd76ba52c1107e5231417
Abdulrahman Saber <abdulrahmansaber120@gmail.com>
2024-08-25 20:06:26 +0300

    Initial Project structure

---
+++ src/schema/index.ts
@@ -0,0 +1,15 @@
+import zod from "zod";
+
+export const UserSchema = {
+  login: zod.object({
+    email: zod.string().email({ message: "Email address is required" }),
+    password: zod.string().min(1, { message: "Password is required" }),
+  }),
+  register: zod.object({
+    email: zod.string().email({ message: "Email is required" }),
+    password: zod.string().min(1, { message: "Password is required" }),
+    name: zod.string().min(1, { message: "Name is required" }),
+    jobTitle: zod.string().min(1, { message: "Job Title is required" }),
+    jobDescription: zod.string().min(1, { message: "Job Description is required" })
+  })
+}
